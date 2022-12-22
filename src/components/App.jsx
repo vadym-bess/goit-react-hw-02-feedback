175093c881655db516134a15c06332577e26a405
Vadym <besarabvadim2@gmail.com>
2022-12-22 13:37:33 +0200

    ljhfglhlghg

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,10 @@
+import React from 'react';
+import { Section } from './Section/Section';
+
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+  return ( <div>
+      <Section />
+         </div>
+);
 };
